f799e209afb025ac2f9a12d262144e6ff3e7f173
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2024-12-26 20:33:48 +0530

    Update File

--- server/app.js
+++ server/app.js
@@ -14,8 +14,21 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
-app.use(cors({ origin: 'https://code-hire.vercel.app' }));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://code-hire.vercel.app",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("public"));
